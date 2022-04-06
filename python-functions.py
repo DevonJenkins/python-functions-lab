820063f88b37eaeef40134355835fc965686e9a2
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-05 21:07:47 -0400

    complete exercises 1 - 3

---
+++ python-functions.py
@@ -0,0 +1,42 @@
+
+
+# def sum_to():
+#   #accept a single number(n) and return the sum of integers that count up to that number(n)
+#   n = int(input("Enter a number: "))
+#   i=1
+#   sum = 0
+#   while (i<=n):
+#     sum = sum + i
+#     i = i + 1
+#   print(f'your number is {n}. The sum of all of the numbers that increment up to that number is {sum}')
+# sum_to()
+
+
+# def largest():
+#   #num = input('enter a list of numbers: ')
+#   list = [2, 4, 5, 6, 7 ]  #num.split(',')
+#   largest_number = list[0]
+
+#   for n in list:
+#     if n > largest_number: 
+#       largest_number = n
+
+#   print(largest_number)
+
+# largest()  
+
+# print('Hello and welcome to the letter counter')
+# str = input("Enter your word: ")
+# letter = input("Input the letter you're looking for: ")
+# def occurrences(str, letter):
+
+#   count = 0
+#   for i in str:
+#     if i == letter: 
+#       count = count + 1
+#   print(f'Count of {letter} in {str} is {count} ')
+# occurrences(str, letter)
+
+# def my_sum(a, b):
+#   return a + b 
+
